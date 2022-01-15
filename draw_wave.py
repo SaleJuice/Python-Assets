13e3b23feda3a919b01d9f6365a44b4d62c1e79e
SaleJuice <43096769+SaleJuice@users.noreply.github.com>
2022-01-15 11:26:27 +0800

    Update draw_wave.py

--- draw_wave.py
+++ draw_wave.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         index = 0
         while True:
             x.append(index)  # 添加i到x轴的数据中
-            y.append(uesr_data)  # 添加i的平方到y轴的数据中
+            y.append(user_data)  # 添加i的平方到y轴的数据中
             plt.clf()  # 清除之前画的图
             plt.plot(x, y)  # 画出当前x列表和y列表中的值的图形
             plt.pause(0.001)  # 暂停一段时间，不然画的太快会卡住显示不出来
